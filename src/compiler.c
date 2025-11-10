8aa03c4f8ccc6bdba91d723efd7e36b690a66966
Gappylul <szakosbalazs2007@gmail.com>
2025-11-10 19:23:17 +0100

    Scanner started code structure refined

---
+++ src/compiler.c
@@ -0,0 +1,13 @@
+//
+// Created by Balázs Szakos on 2025. 11. 10..
+//
+
+#include <stdio.h>
+
+#include "common.h"
+#include "compiler.h"
+#include "scanner.h"
+
+void compile(const char* source) {
+    initScanner(source);
+}
