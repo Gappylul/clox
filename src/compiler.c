832f09f0f1536824ecbcea16714590b1a56c0ab9
Gappylul <szakosbalazs2007@gmail.com>
2025-11-10 19:54:58 +0100

    Lexical grammar

--- src/compiler.c
+++ src/compiler.c
@@ -10,4 +10,16 @@
 
 void compile(const char* source) {
     initScanner(source);
+    int line = -1;
+    for (;;) {
+        Token token = scanToken();
+        if (token.line != line) {
+            printf("%4d", token.line);
+        } else {
+            printf("\t| ");
+        }
+        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+
+        if (token.type == TOKEN_EOF) break;
+    }
 }
